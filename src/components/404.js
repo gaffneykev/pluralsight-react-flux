c41930270751f1a04c4c43db85eadd69c06db57d
Kevin Gaffney <kevin.gaffney@workday.com>
2018-01-12 16:04:42 +0000

    Creating a form

--- src/components/404.js
+++ src/components/404.js
@@ -14,6 +14,6 @@ var NotFoundPage = React.createClass({
       </div>
     );
   }
-})
+});
 
 module.exports = NotFoundPage;
--- src/components/common/header.js
+++ src/components/common/header.js
@@ -10,7 +10,7 @@ var Header = React.createClass({
       <nav className="navbar navbar-default">
         <div className="container-fluid">
           <a href="/" className="navbar-brand">
-            <img src="images/pluralsight-logo.png" />
+            <img src="images/pluralsight-logo.png" className="hidden"/>
           </a>
           <ul className="nav navbar-nav">
             <li><Link to="app">Home</Link></li>
