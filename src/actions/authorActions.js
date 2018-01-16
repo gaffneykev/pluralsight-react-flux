e3821c292ed13b172cda001116b6b00a0f57a9f9
Kevin Gaffney <kevin.gaffney@workday.com>
2018-01-16 09:40:12 +0000

    Add actions

---
+++ src/actions/authorActions.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var Dispatcher = require('../dispatcher/appDispatcher');
+var AuthorAPI = require('../api/AuthorAPI');
+var ActionTypes = require('../constants/ActionTypes');
+
+var AuthorActions = {
+  createAuthor: function(author) {
+    var newAuthor = AuthorAPI.saveAuthor(author);
+    //Hey dispatcher go tell all the stores that an author was just created
+    Dispatcher.dispath({
+      actionType: ActionTypes.CREATE_AUTHOR,
+      author: newAuthor
+    });
+  }
+};
+
+module.exports = AuthorActions;
